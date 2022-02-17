5d4d2850c2bce5a19746485f729e63cb7a3f85f2
Silence.L <228699456@qq.com>
2022-02-18 00:17:45 +0800

    developer-1.2.0

---
+++ XLTestDemo/Controller/ThreeViewController.h
@@ -0,0 +1,16 @@
+//
+//  ThreeViewController.h
+//  XLTestDemo
+//
+//  Created by 甘延娇 on 2022/2/18.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ThreeViewController : ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XLTestDemo/Controller/ThreeViewController.m
@@ -0,0 +1,22 @@
+//
+//  ThreeViewController.m
+//  XLTestDemo
+//
+//  Created by 甘延娇 on 2022/2/18.
+//
+
+#import "ThreeViewController.h"
+
+@interface ThreeViewController ()
+
+@end
+
+@implementation ThreeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //这是developer-1.2.0
+}
+
+@end
